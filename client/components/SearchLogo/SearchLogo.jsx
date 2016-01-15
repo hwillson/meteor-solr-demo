a25666c7e21d5a3b9524aa9acc3e4cd0797d87be
hwillson <hugh@octonary.com>
2016-01-15 09:18:13 -0500

    Wired up basic mobile styles.

--- client/components/SearchLogo/SearchLogo.jsx
+++ client/components/SearchLogo/SearchLogo.jsx
@@ -2,7 +2,7 @@ SearchLogo = React.createClass({
 
   render() {
     return (
-      <div className="navbar-header search-logo">
+      <div className="navbar-header search-logo hidden-xs hidden-sm">
         <a className="navbar-brand" href="#">
           <img src="/images/search_logo.png" alt="Search logo" />
         </a>
