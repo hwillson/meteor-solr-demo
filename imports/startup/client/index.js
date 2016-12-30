e4f87e07177982fb602427c958996603d46ad12e
Hugh Willson <hugh@octonary.com>
2016-12-30 13:55:27 -0500

    Removed flow router; Added react router.

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,1 +1,13 @@
-import './routes';
+/* global document */
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
+import Search from '../../ui/pages/Search';
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={Search} />
+  </Router>
+), document.getElementById('app'));
